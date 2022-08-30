e737006b750f174d1a8fbed704c0502839851d95
Swajan Baruah <70163983+swajan-75@users.noreply.github.com>
2022-08-30 16:10:48 +0600

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import LoginPage.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException.*;
+public class MainClass {
+    public static void main(String[] args) { 
+    
+        LoginPage obj = new LoginPage();
+}
+}
